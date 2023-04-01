791f6f71720d742871651a786ecd5dce8b1bb253
qb <saschap@mail.de>
2023-04-02 00:58:39 +0200

    add onebyte wallhack for windows

--- examples/windows/csgo_wallhack.py
+++ examples/windows/csgo_wallhack.py
@@ -5,3 +5,4 @@ from pyMeow import *
 pm = open_process(processName="csgo.exe")
 address = aob_scan_module(pm, "client.dll", "33 C0 83 FA ?? B9 20")[0]
 w_byte(pm, address + 4, 2 if r_byte(pm, address) == 1 else 1)
+close_process(pm)
