b2907c7e058c24d26a88d06f60d8515aaadb6790
qb <saschap@mail.de>
2023-04-02 07:55:53 +0200

    sound module by @IagoBeuller

--- pyMeow.nim
+++ pyMeow.nim
@@ -11,4 +11,5 @@ import src/[
   gui,
   vec,
   pixel,
+  sound,
 ]
---
+++ src/sound.nim
@@ -0,0 +1,47 @@
+import 
+  nimpy, tables,
+  nimraylib_now as rl
+
+pyExportModule("pyMeow")
+
+type
+  SoundObj = object
+    id: int
+    sound: Sound
+
+var
+  curSoundId = -1
+  soundTable: Table[int, SoundObj]
+
+proc loadSound(fileName: string): int {.exportpy: "load_sound".} =
+  inc curSoundId
+  soundTable[curSoundId] = SoundObj(id: curSoundId, sound: rl.loadSound(fileName))
+  result = curSoundId
+
+proc unloadSound(soundId: int) {.exportpy: "unload_sound".} =
+  rl.unloadSound(soundTable[soundId].sound)
+  soundTable.del(soundId)
+
+proc playSound(soundId: int) {.exportpy: "play_sound".} =
+  rl.playSound(soundTable[soundId].sound)
+
+proc pauseSound(soundId: int) {.exportpy: "pause_sound".} =
+  rl.pauseSound(soundTable[soundId].sound)
+
+proc resumeSound(soundId: int) {.exportpy: "resume_sound".} =
+  rl.resumeSound(soundTable[soundId].sound)
+
+proc stopSound(soundId: int) {.exportpy: "stop_sound".} =
+  rl.stopSound(soundTable[soundId].sound)
+
+proc playMultiSound(soundId: int) {.exportpy: "play_multisound".} =
+  rl.playSoundMulti(soundTable[soundId].sound)
+
+proc stopMultiSound() {.exportpy: "stop_multisound".} =
+  rl.stopSoundMulti()
+
+proc setSoundVolume(soundId: int, volume: int): bool {.exportpy: "set_sound_volume".} =
+  rl.setSoundVolume(soundTable[soundId].sound, volume.float / 100.0)
+
+proc isSoundPlaying(soundId: int): bool {.exportpy: "is_sound_playing".} =
+  rl.isSoundPlaying(soundTable[soundId].sound)
