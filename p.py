a64f3d6f8cf488b003913d5bd1ff0f32d15785f3
Hypnootika <34210817+Hypnootika@users.noreply.github.com>
2023-11-03 06:20:47 +0100

    Delete p.py

--- p.py
+++
@@ -1,9 +0,0 @@
-from pyMeow import *
-from time import sleep
-pyMeow.overlay_init()
-pyMeow.set_fps(60)
-while pyMeow.overlay_loop():
-    pyMeow.begin_drawing()
-    pyMeow.draw_fps(10, 10)
-
-    pyMeow.end_drawing()
