0a3c8441ebd945362d8178daebe165dd1c89f77b
qb <34208612+qb-0@users.noreply.github.com>
2024-08-19 20:33:26 +0200

    Update memcore.nim

--- src/memcore.nim
+++ src/memcore.nim
@@ -219,7 +219,7 @@ proc openProcess(process: PyObject, debug: bool = false): Process {.exportpy: "o
   result.debug = debug
   result.pid = sPid
   result.name = getProcessName(sPid)
-  var modules = toSeq(enumModules(result))
+  let modules = enumModules(result).toSeq()[0]
   if modules.len > 0:
     result.base = modules[0].base
 
